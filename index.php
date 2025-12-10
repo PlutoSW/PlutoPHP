7f3425e75edc7aada894319c80edc124bd770d1c
PlutoSW <iletisim@yasinersoy.com.tr>
2025-12-10 17:23:08 +0300

    vendor register

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
+}
+
 require_once __DIR__ . '/core/bootstrap.php';
 
 $app = new Pluto\Application();
